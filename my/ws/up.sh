f6f1ae3acf2f925697eb8658651178fedc833a21
mytcgd <131883869+mytcgd@users.noreply.github.com>
2024-01-03 01:07:26 +0800

    Add files via upload

--- my/ws/up.sh
+++ my/ws/up.sh
@@ -30,7 +30,11 @@ upload_url_data() {
 if [ -z "$ARGO_AUTH" ] && [ -z "$ARGO_DOMAIN" ]; then
   [ -s ${FILE_PATH}/boot.log ] && export ARGO_DOMAIN=$(cat ${FILE_PATH}/boot.log | grep -o "info.*https://.*trycloudflare.com" | sed "s@.*https://@@g" | tail -n 1)
 fi
-country_abbreviation=$(cat ${FILE_PATH}/country.txt)
+
+if [ -e ${FILE_PATH}/country.txt ]; then
+  country_abbreviation=$(cat ${FILE_PATH}/country.txt)
+fi
+
 export VL_URL="vless://${UUID}@${ARGO_DOMAIN}:443?host=${ARGO_DOMAIN}&path=%2F&type=ws&encryption=none&security=tls&sni=${ARGO_DOMAIN}#vless-${country_abbreviation}-${SUB_NAME}"
 
 if [ -n "$SUB_URL" ]; then
