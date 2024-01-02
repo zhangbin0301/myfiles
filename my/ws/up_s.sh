e17bb278d7875cab73783e2daa3a7589acbd8e58
mytcgd <131883869+mytcgd@users.noreply.github.com>
2024-01-03 01:13:02 +0800

    Create up_s.sh

---
+++ my/ws/up_s.sh
@@ -0,0 +1,43 @@
+#!/usr/bin/env bash
+
+while true
+do
+# 上传订阅
+upload_url_data() {
+    if [ $# -lt 3 ]; then
+        return 1
+    fi
+
+    UPLOAD_URL="$1"
+    URL_NAME="$2"
+    URL_TO_UPLOAD="$3"
+
+    # 检查curl命令是否存在
+    if command -v curl &> /dev/null; then
+
+        curl -s -o /dev/null -X POST -H "Content-Type: application/json" -d "{\"URL_NAME\": \"$URL_NAME\", \"URL\": \"$URL_TO_UPLOAD\"}" "$UPLOAD_URL"
+
+    # 检查wget命令是否存在
+    elif command -v wget &> /dev/null; then
+
+        echo "{\"URL_NAME\": \"$URL_NAME\", \"URL\": \"$URL_TO_UPLOAD\"}" | wget --quiet --post-data=- --header="Content-Type: application/json" "$UPLOAD_URL" -O -
+
+    else
+        echo "Both curl and wget are not installed. Please install one of them to upload data."
+    fi
+}
+
+if [ -z "$ARGO_AUTH" ] && [ -z "$ARGO_DOMAIN" ]; then
+  [ -s ${FILE_PATH}/boot.log ] && export ARGO_DOMAIN=$(cat ${FILE_PATH}/boot.log | grep -o "info.*https://.*trycloudflare.com" | sed "s@.*https://@@g" | tail -n 1)
+fi
+
+if [ -e ${FILE_PATH}/country.txt ]; then
+  country_abbreviation=$(cat ${FILE_PATH}/country.txt)
+fi
+
+export VL_URL="vless://${UUID}@${ARGO_DOMAIN}:443?host=${ARGO_DOMAIN}&path=%2F&type=ws&encryption=none&security=tls&sni=${ARGO_DOMAIN}#vless-${country_abbreviation}-${SUB_NAME}"
+upload_url_data "${SUB_URL}" "${SUB_NAME}" "${VL_URL}"
+# echo "upload ok!"
+
+sleep 300
+done
