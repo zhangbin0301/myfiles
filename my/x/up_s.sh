3514991b5e946262392d2f06d36b8b5df26fe686
mytcgd <131883869+mytcgd@users.noreply.github.com>
2024-01-03 01:23:45 +0800

    Add files via upload

--- my/x/up_s.sh
+++ my/x/up_s.sh
@@ -31,7 +31,10 @@ if [ -z "$ARGO_AUTH" ] && [ -z "$ARGO_DOMAIN" ]; then
   [ -s ${FILE_PATH}/boot.log ] && export ARGO_DOMAIN=$(cat ${FILE_PATH}/boot.log | grep -o "info.*https://.*trycloudflare.com" | sed "s@.*https://@@g" | tail -n 1)
 fi
 
-# country_abbreviation=$(curl -s https://speed.cloudflare.com/meta | tr ',' '\n' | grep -Po '"country"\s*:\s*"\K([^"]*)')
+if [ -e ${FILE_PATH}/country.txt ]; then
+  country_abbreviation=$(cat ${FILE_PATH}/country.txt)
+fi
+
 export VM_URL="vmess://$(echo "$VMESS" | base64 | tr -d '\n')"
 export VL_URL="vless://${UUID}@${CF_IP}:443?host=${ARGO_DOMAIN}&path=%2F${VLESS_WSPATH}%3Fed%3D2048&type=ws&encryption=none&security=tls&sni=${ARGO_DOMAIN}#vless-${country_abbreviation}-${SUB_NAME}"
 # upload_url_data "${SUB_URL}" "${SUB_NAME}" "${VM_URL}"
