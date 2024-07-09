8afe88ce511b64b181b3c42f0b37bb8a17da3e0a
mytcgd <131883869+mytcgd@users.noreply.github.com>
2024-07-10 03:16:48 +0800

    Update up_v.sh

--- my/x/up_v.sh
+++ my/x/up_v.sh
@@ -16,12 +16,10 @@ upload_url_data() {
     URL_NAME="$2"
     URL_TO_UPLOAD="$3"
 
-    # 检查curl命令是否存在
     if command -v curl &> /dev/null; then
 
         curl -s -o /dev/null -X POST -H "Content-Type: application/json" -d "{\"URL_NAME\": \"$URL_NAME\", \"URL\": \"$URL_TO_UPLOAD\"}" "$UPLOAD_URL"
 
-    # 检查wget命令是否存在
     elif command -v wget &> /dev/null; then
 
         echo "{\"URL_NAME\": \"$URL_NAME\", \"URL\": \"$URL_TO_UPLOAD\"}" | wget --quiet --post-data=- --header="Content-Type: application/json" "$UPLOAD_URL" -O -
@@ -35,11 +33,10 @@ if [ -z "$ARGO_AUTH" ] && [ -z "$ARGO_DOMAIN" ]; then
   [ -s ${FILE_PATH}/argo.log ] && export ARGO_DOMAIN=$(cat ${FILE_PATH}/argo.log | grep -o "info.*https://.*trycloudflare.com" | sed "s@.*https://@@g" | tail -n 1)
 fi
 
-export VM_URL="vmess://$(echo "$VMESS" | base64 | tr -d '\n')"
+# export VM_URL="vmess://$(echo "$VMESS" | base64 | tr -d '\n')"
 export VL_URL="vless://${UUID}@${CF_IP}:${CFPORT}?host=${ARGO_DOMAIN}&path=%2F${VLESS_WSPATH}%3Fed%3D2048&type=ws&encryption=none&security=tls&sni=${ARGO_DOMAIN}#vless-${country_abbreviation}-${SUB_NAME}"
 # upload_url_data "${SUB_URL}" "${SUB_NAME}" "${VM_URL}"
 upload_url_data "${SUB_URL}" "${SUB_NAME}" "${VL_URL}"
-# echo "upload ok!"
 
   if [ -e ${FILE_PATH}/argo ]; then
     if ! pgrep -f argo > /dev/null; then
