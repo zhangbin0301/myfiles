08b375823641104477779b54b84e15bd464674e2
mytcgd <131883869+mytcgd@users.noreply.github.com>
2024-07-21 18:02:57 +0800

    Update up_v.sh

--- my/x/up_v.sh
+++ my/x/up_v.sh
@@ -24,28 +24,30 @@ if source ${FILE_PATH}/env_vars.sh; then
       fi
   }
 
-  if [ -z "$ARGO_AUTH" ] && [ -z "$ARGO_DOMAIN" ]; then
-    [ -s ${FILE_PATH}/argo.log ] && export ARGO_DOMAIN=$(cat ${FILE_PATH}/argo.log | grep -o "info.*https://.*trycloudflare.com" | sed "s@.*https://@@g" | tail -n 1)
-  fi
+  if [ ${openserver} -eq 1 ]; then
+    if [ -z "$ARGO_AUTH" ] && [ -z "$ARGO_DOMAIN" ]; then
+      [ -s ${FILE_PATH}/argo.log ] && export ARGO_DOMAIN=$(cat ${FILE_PATH}/argo.log | grep -o "info.*https://.*trycloudflare.com" | sed "s@.*https://@@g" | tail -n 1)
+    fi
+    VMESS="{ \"v\": \"2\", \"ps\": \"vmess-${country_abbreviation}-${SUB_NAME}\", \"add\": \"${CF_IP}\", \"port\": \"${CFPORT}\", \"id\": \"${UUID}\", \"aid\": \"0\", \"scy\": \"none\", \"net\": \"ws\", \"type\": \"none\", \"host\": \"${ARGO_DOMAIN}\", \"path\": \"/${VMESS_WSPATH}?ed=2048\", \"tls\": \"tls\", \"sni\": \"${ARGO_DOMAIN}\", \"alpn\": \"\" }"
+    vmess_url="vmess://$(echo "$VMESS" | base64 | tr -d '\n')"
+    vless_url="vless://${UUID}@${CF_IP}:${CFPORT}?host=${ARGO_DOMAIN}&path=%2F${VLESS_WSPATH}%3Fed%3D2048&type=ws&encryption=none&security=tls&sni=${ARGO_DOMAIN}#vless-${country_abbreviation}-${SUB_NAME}"
+    export UPLOAD_DATA="$vless_url"
+    # export UPLOAD_DATA="$vmess_url\n$vless_url"
 
-  VMESS="{ \"v\": \"2\", \"ps\": \"vmess-${country_abbreviation}-${SUB_NAME}\", \"add\": \"${CF_IP}\", \"port\": \"${CFPORT}\", \"id\": \"${UUID}\", \"aid\": \"0\", \"scy\": \"none\", \"net\": \"ws\", \"type\": \"none\", \"host\": \"${ARGO_DOMAIN}\", \"path\": \"/${VMESS_WSPATH}?ed=2048\", \"tls\": \"tls\", \"sni\": \"${ARGO_DOMAIN}\", \"alpn\": \"\" }"
-  vmess_url="vmess://$(echo "$VMESS" | base64 | tr -d '\n')"
-  vless_url="vless://${UUID}@${CF_IP}:${CFPORT}?host=${ARGO_DOMAIN}&path=%2F${VLESS_WSPATH}%3Fed%3D2048&type=ws&encryption=none&security=tls&sni=${ARGO_DOMAIN}#vless-${country_abbreviation}-${SUB_NAME}"
-  export UPLOAD_DATA="$vless_url"
-  # export UPLOAD_DATA="$vmess_url\n$vless_url"
-  # echo -e "${UPLOAD_URL}"
-
-  upload_url_data "${SUB_URL}" "${SUB_NAME}" "${UPLOAD_DATA}"
-
-  if [ -e ${FILE_PATH}/argo ]; then
-    if ! pgrep -f argo > /dev/null; then
-      if [ -f /etc/alpine-release ]; then
-        systemctl start argo
-      else
-        systemctl start argo.service
+    if [ -e ${FILE_PATH}/argo ]; then
+      if ! pgrep -f argo > /dev/null; then
+        if [ -f /etc/alpine-release ]; then
+          systemctl start argo
+        else
+          systemctl start argo.service
+        fi
       fi
     fi
+  else
+    export UPLOAD_DATA="vless://${UUID}@${MYIP}:${REAL_PORT}?encryption=none&flow=xtls-rprx-vision&security=reality&sni=${SNI}&fp=chrome&pbk=${PublicKey}&sid=${shortid}&type=tcp&headerType=none#${country_abbreviation}-${SUB_NAME}"
   fi
+
+  upload_url_data "${SUB_URL}" "${SUB_NAME}" "${UPLOAD_DATA}"
 
   if [ -e ${FILE_PATH}/web ]; then
     if ! pgrep -f web > /dev/null; then
