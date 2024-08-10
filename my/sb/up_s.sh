7e48fc76facd8c3221387ec8a2c2e8173f656541
mytcgd <131883869+mytcgd@users.noreply.github.com>
2024-08-11 07:22:54 +0800

    Update up_s.sh

--- my/sb/up_s.sh
+++ my/sb/up_s.sh
@@ -30,7 +30,7 @@ fi
 
 vmess_url="vmess://$(echo "$VMESS" | base64 | tr -d '\n')"
 hysteria_url="hysteria2://${UUID}@${IP}:${H_PORT}/?sni=www.bing.com&alpn=h3&insecure=1#${ISP}-${SUB_NAME}"
-tuic_url="tuic://${UUID}:@${IP}:${TUIC_PORT}?sni=www.bing.com&alpn=h3&congestion_control=bbr#${ISP}-${SUB_NAME}"
+tuic://${UUID}:${password}@${IP}:${TUIC_PORT}?sni=www.bing.com&congestion_control=bbr&udp_relay_mode=native&alpn=h3&allow_insecure=1#${ISP}-${SUB_NAME}
 reality_url="vless://${UUID}@${IP}:${SERVER_PORT}?encryption=none&flow=xtls-rprx-vision&security=reality&sni=${SNI}&fp=chrome&pbk=${public_key}&type=tcp&headerType=none#${ISP}-${SUB_NAME}"
 
 if [ -n "$openreality" ] && [ "$openreality" != "0" ]; then
