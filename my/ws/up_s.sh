161955c117c6f341f6aea92eb8c922b6a8ec1376
mytcgd <131883869+mytcgd@users.noreply.github.com>
2024-08-11 21:19:56 +0800

    Update up_s.sh

--- my/ws/up_s.sh
+++ my/ws/up_s.sh
@@ -1,5 +1,11 @@
 #!/usr/bin/env bash
 
+check_hostname_change() {
+  if [ -z "$ARGO_AUTH" ] && [ -z "$ARGO_DOMAIN" ]; then
+    [ -s ${FILE_PATH}/boot.log ] && export ARGO_DOMAIN=$(cat ${FILE_PATH}/boot.log | grep -o "info.*https://.*trycloudflare.com" | sed "s@.*https://@@g" | tail -n 1)
+  fi
+}
+
 while true
 do
 # 上传订阅
@@ -27,15 +33,32 @@ upload_url_data() {
     fi
 }
 
-if [ -z "$ARGO_AUTH" ] && [ -z "$ARGO_DOMAIN" ]; then
-  [ -s ${FILE_PATH}/boot.log ] && export ARGO_DOMAIN=$(cat ${FILE_PATH}/boot.log | grep -o "info.*https://.*trycloudflare.com" | sed "s@.*https://@@g" | tail -n 1)
-fi
+check_hostname_change
 
 export UPLOAD_DATA="vless://${UUID}@${CF_IP}:${CFPORT}?host=${ARGO_DOMAIN}&path=%2F&type=ws&encryption=none&security=tls&sni=${ARGO_DOMAIN}#${country_abbreviation}-${SUB_NAME}"
 # export UPLOAD_DATA="vless://${UUID}@${ARGO_DOMAIN}:${CFPORT}?host=${ARGO_DOMAIN}&path=%2F&type=ws&encryption=none&security=tls&sni=${ARGO_DOMAIN}#${country_abbreviation}-${SUB_NAME}"
 
-upload_url_data "${SUB_URL}" "${SUB_NAME}" "${UPLOAD_DATA}"
-# echo "upload ok!"
+if [ -n "$SUB_URL" ]; then
+  upload_url_data "${SUB_URL}" "${SUB_NAME}" "${UPLOAD_DATA}"
+  # echo "upload ok!"
+fi
+
+if [ -n "$openkeepalive" ] && [ "$openkeepalive" != "0" ]; then
+  if [[ $(pidof server) ]]; then
+    hint "server is already running !"
+  else
+    info "server runs again !"
+    run_server
+    build_urls
+  fi
+
+  if [[ $(pidof npm) ]]; then
+    hint "npm is already running !"
+  else
+    info "npm runs again !"
+    run_npm
+  fi
+fi
 
 sleep 300
 done
