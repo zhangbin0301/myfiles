10ab409ec27bcb6d2e5dc5a3ed361df72880b4dd
mytcgd <131883869+mytcgd@users.noreply.github.com>
2024-08-14 05:07:37 +0800

    Update up_v_sb.sh

--- my/sb/up_v_sb.sh
+++ my/sb/up_v_sb.sh
@@ -1,42 +1,39 @@
 #!/bin/bash
 
-export FILE_PATH=${FILE_PATH:-'/root/argox'}
-
-if source ${FILE_PATH}/env_vars.sh; then
+if source /root/env.yml; then
   while true
   do
-  # up
+
   upload_url_data() {
-      if [ $# -lt 3 ]; then
-          return 1
-      fi
+    if [ $# -lt 3 ]; then
+      return 1
+    fi
 
-      UPLOAD_URL="$1"
-      URL_NAME="$2"
-      URL_TO_UPLOAD="$3"
+    UPLOAD_URL="$1"
+    URL_NAME="$2"
+    URL_TO_UPLOAD="$3"
 
-      if command -v curl &> /dev/null; then
-          curl -s -o /dev/null -X POST -H "Content-Type: application/json" -d "{\"URL_NAME\": \"$URL_NAME\", \"URL\": \"$URL_TO_UPLOAD\"}" "$UPLOAD_URL"
-      elif command -v wget &> /dev/null; then
-          echo "{\"URL_NAME\": \"$URL_NAME\", \"URL\": \"$URL_TO_UPLOAD\"}" | wget --quiet --post-data=- --header="Content-Type: application/json" "$UPLOAD_URL" -O -
-      else
-        echo "Both curl and wget are not installed. Please install one of them to upload data."
-      fi
+    # 检查curl命令是否存在
+    if command -v curl &> /dev/null; then
+      curl -s -o /dev/null -X POST -H "Content-Type: application/json" -d "{\"URL_NAME\": \"$URL_NAME\", \"URL\": \"$URL_TO_UPLOAD\"}" "$UPLOAD_URL"
+    # 检查wget命令是否存在
+    elif command -v wget &> /dev/null; then
+      echo "{\"URL_NAME\": \"$URL_NAME\", \"URL\": \"$URL_TO_UPLOAD\"}" | wget --quiet --post-data=- --header="Content-Type: application/json" "$UPLOAD_URL" -O -
+    else
+      echo "Both curl and wget are not installed. Please install one of them to upload data."
+    fi
   }
 
-  if [ -z "$ARGO_AUTH" ] && [ -z "$ARGO_DOMAIN" ]; then
-    [ -s ${FILE_PATH}/argo.log ] && export ARGO_DOMAIN=$(cat ${FILE_PATH}/argo.log | grep -o "info.*https://.*trycloudflare.com" | sed "s@.*https://@@g" | tail -n 1)
-    # [ -s ${FILE_PATH}/argo.log ] && export ARGO_DOMAIN=$(cat ${FILE_PATH}/argo.log | grep -o "https://.*trycloudflare.com" | tail -n 1 | sed 's/https:\/\///')
-  fi
+  [ -s ${FILE_PATH}/argo.log ] && export ARGO_DOMAIN=$(cat ${FILE_PATH}/argo.log | grep -o "info.*https://.*trycloudflare.com" | sed "s@.*https://@@g" | tail -n 1)
+  # [ -s ${FILE_PATH}/argo.log ] && export ARGO_DOMAIN=$(cat argo.log | grep -oP '(?<=\|)(?!https://).*(?=\.com|$)' | tail -n 1)
+  # [ -s ${FILE_PATH}/argo.log ] && export ARGO_DOMAIN=$(cat ${FILE_PATH}/argo.log | grep -o "https://.*trycloudflare.com" | tail -n 1 | sed 's/https:\/\///')
 
-  sleep 3
-
-  VMESS="{ \"v\": \"2\", \"ps\": \"${ISP}-${SUB_NAME}\", \"add\": \"${CFIP}\", \"port\": \"${CFPORT}\", \"id\": \"${UUID}\", \"aid\": \"0\", \"scy\": \"none\", \"net\": \"ws\", \"type\": \"none\", \"host\": \"${ARGO_DOMAIN}\", \"path\": \"/vmess?ed=2048\", \"tls\": \"tls\", \"sni\": \"${ARGO_DOMAIN}\", \"alpn\": \"\", \"fp\": \"randomized\"}"
+  VMESS="{ \"v\": \"2\", \"ps\": \"${country_abbreviation}-${SUB_NAME}\", \"add\": \"${CF_IP}\", \"port\": \"${CFPORT}\", \"id\": \"${UUID}\", \"aid\": \"0\", \"scy\": \"none\", \"net\": \"ws\", \"type\": \"none\", \"host\": \"${ARGO_DOMAIN}\", \"path\": \"/vmess?ed=2048\", \"tls\": \"tls\", \"sni\": \"${ARGO_DOMAIN}\", \"alpn\": \"\", \"fp\": \"randomized\"}"
 
   vmess_url="vmess://$(echo "$VMESS" | base64 | tr -d '\n')"
-  hysteria_url="hysteria2://${UUID}@${IP}:${H_PORT}/?sni=www.bing.com&alpn=h3&insecure=1#${ISP}-${SUB_NAME}"
-  tuic_url="tuic://${UUID}:${tuicpass}@${IP}:${TUIC_PORT}?sni=www.bing.com&congestion_control=bbr&udp_relay_mode=native&alpn=h3&allow_insecure=1#${ISP}-${SUB_NAME}"
-  reality_url="vless://${UUID}@${IP}:${SERVER_PORT}?encryption=none&flow=xtls-rprx-vision&security=reality&sni=${SNI}&fp=chrome&pbk=${public_key}&type=tcp&headerType=none#${ISP}-${SUB_NAME}"
+  hysteria_url="hysteria2://${UUID}@${MYIP}:${HY2_PORT}/?sni=www.bing.com&alpn=h3&insecure=1#${country_abbreviation}-${SUB_NAME}"
+  tuic_url="tuic://${UUID}:${tuicpass}@${MYIP}:${TUIC_PORT}?sni=www.bing.com&congestion_control=bbr&udp_relay_mode=native&alpn=h3&allow_insecure=1#${country_abbreviation}-${SUB_NAME}"
+  reality_url="vless://${UUID}@${MYIP}:${REAL_PORT}?encryption=none&flow=xtls-rprx-vision&security=reality&sni=${SNI}&fp=chrome&pbk=${public_key}&type=tcp&headerType=none#${country_abbreviation}-${SUB_NAME}"
 
   if [ -n "$openreality" ] && [ "$openreality" != "0" ]; then
     export UPLOAD_DATA="$vmess_url\n$hysteria_url\n$tuic_url\n$reality_url"
@@ -48,6 +45,7 @@ if source ${FILE_PATH}/env_vars.sh; then
   # echo "upload ok !"
 
   upload_url_data "${SUB_URL}" "${SUB_NAME}" "${UPLOAD_DATA}"
+  # echo "upload ok!"
 
   sleep 100
   done
