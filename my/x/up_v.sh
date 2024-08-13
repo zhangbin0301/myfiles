984e54ffb417796ed428b500671e168c20647bd6
mytcgd <131883869+mytcgd@users.noreply.github.com>
2024-08-14 03:56:09 +0800

    Update up_v.sh

--- my/x/up_v.sh
+++ my/x/up_v.sh
@@ -1,13 +1,12 @@
 #!/usr/bin/env bash
 
 if source /root/env.yml; then
+  while true
+  do
 
-while true
-do
-# 上传订阅
-upload_url_data() {
+  upload_url_data() {
     if [ $# -lt 3 ]; then
-        return 1
+      return 1
     fi
 
     UPLOAD_URL="$1"
@@ -16,34 +15,38 @@ upload_url_data() {
 
     # 检查curl命令是否存在
     if command -v curl &> /dev/null; then
-
-        curl -s -o /dev/null -X POST -H "Content-Type: application/json" -d "{\"URL_NAME\": \"$URL_NAME\", \"URL\": \"$URL_TO_UPLOAD\"}" "$UPLOAD_URL"
-
+      curl -s -o /dev/null -X POST -H "Content-Type: application/json" -d "{\"URL_NAME\": \"$URL_NAME\", \"URL\": \"$URL_TO_UPLOAD\"}" "$UPLOAD_URL"
     # 检查wget命令是否存在
     elif command -v wget &> /dev/null; then
-
-        echo "{\"URL_NAME\": \"$URL_NAME\", \"URL\": \"$URL_TO_UPLOAD\"}" | wget --quiet --post-data=- --header="Content-Type: application/json" "$UPLOAD_URL" -O -
-
+      echo "{\"URL_NAME\": \"$URL_NAME\", \"URL\": \"$URL_TO_UPLOAD\"}" | wget --quiet --post-data=- --header="Content-Type: application/json" "$UPLOAD_URL" -O -
     else
-        echo "Both curl and wget are not installed. Please install one of them to upload data."
+      echo "Both curl and wget are not installed. Please install one of them to upload data."
     fi
-}
+  }
 
-[ -s ${FILE_PATH}/argo.log ] && export ARGO_DOMAIN=$(cat ${FILE_PATH}/argo.log | grep -o "info.*https://.*trycloudflare.com" | sed "s@.*https://@@g" | tail -n 1)
-# [ -s ${FILE_PATH}/argo.log ] && export ARGO_DOMAIN=$(cat argo.log | grep -oP '(?<=\|)(?!https://).*(?=\.com|$)' | tail -n 1)
-# [ -s ${FILE_PATH}/argo.log ] && export ARGO_DOMAIN=$(cat ${FILE_PATH}/argo.log | grep -o "https://.*trycloudflare.com" | tail -n 1 | sed 's/https:\/\///')
+  [ -s ${FILE_PATH}/argo.log ] && export ARGO_DOMAIN=$(cat ${FILE_PATH}/argo.log | grep -o "info.*https://.*trycloudflare.com" | sed "s@.*https://@@g" | tail -n 1)
+  # [ -s ${FILE_PATH}/argo.log ] && export ARGO_DOMAIN=$(cat argo.log | grep -oP '(?<=\|)(?!https://).*(?=\.com|$)' | tail -n 1)
+  # [ -s ${FILE_PATH}/argo.log ] && export ARGO_DOMAIN=$(cat ${FILE_PATH}/argo.log | grep -o "https://.*trycloudflare.com" | tail -n 1 | sed 's/https:\/\///')
 
-VMESS="{ \"v\": \"2\", \"ps\": \"vmess-${country_abbreviation}-${SUB_NAME}\", \"add\": \"${CF_IP}\", \"port\": \"${CFPORT}\", \"id\": \"${UUID}\", \"aid\": \"0\", \"scy\": \"none\", \"net\": \"ws\", \"type\": \"none\", \"host\": \"${ARGO_DOMAIN}\", \"path\": \"/${VMESS_WSPATH}?ed=2048\", \"tls\": \"tls\", \"sni\": \"${ARGO_DOMAIN}\", \"alpn\": \"\" }"
+  VMESS="{ \"v\": \"2\", \"ps\": \"vmess-${country_abbreviation}-${SUB_NAME}\", \"add\": \"${CF_IP}\", \"port\": \"${CFPORT}\", \"id\": \"${UUID}\", \"aid\": \"0\", \"scy\": \"none\", \"net\": \"ws\", \"type\": \"none\", \"host\": \"${ARGO_DOMAIN}\", \"path\": \"/${VMESS_WSPATH}?ed=2048\", \"tls\": \"tls\", \"sni\": \"${ARGO_DOMAIN}\", \"alpn\": \"\" }"
 
-VM_URL="vmess://$(echo "$VMESS" | base64 | tr -d '\n')"
-VL_URL="vless://${UUID}@${CF_IP}:${CFPORT}?host=${ARGO_DOMAIN}&path=%2F${VLESS_WSPATH}%3Fed%3D2048&type=ws&encryption=none&security=tls&sni=${ARGO_DOMAIN}#vless-${country_abbreviation}-${SUB_NAME}"
-# UPLOAD_DATA="$VL_URL"
-UPLOAD_DATA="$VM_URL\n$VL_URL"
+  vmess_url="vmess://$(echo "$VMESS" | base64 | tr -d '\n')"
+  vless_url="vless://${UUID}@${CF_IP}:${CFPORT}?host=${ARGO_DOMAIN}&path=%2F${VLESS_WSPATH}%3Fed%3D2048&type=ws&encryption=none&security=tls&sni=${ARGO_DOMAIN}#vless-${country_abbreviation}-${SUB_NAME}"
 
-upload_url_data "${SUB_URL}" "${SUB_NAME}" "${UPLOAD_DATA}"
-# echo "upload ok!"
+  if [ -n "$openreality" ] && [ "$openreality" != "0" ]; then
+    reality_url="vless://${UUID}@${MYIP}:${REAL_PORT}?encryption=none&flow=xtls-rprx-vision&security=reality&sni=${SNI}&fp=chrome&pbk=${PublicKey}&sid=${shortid}&type=tcp&headerType=none#${country_abbreviation}-${SUB_NAME}"
+    export UPLOAD_DATA="$vless_url\n$reality_url"
+  elif [ -z "$openreality" ]; then
+    export UPLOAD_DATA="$vless_url"
+    # export UPLOAD_DATA="$vmess_url\n$vless_url"
+  else
+    export UPLOAD_DATA="$vless_url"
+    # export UPLOAD_DATA="$vmess_url\n$vless_url"
+  fi
 
-sleep 100
-done
+  upload_url_data "${SUB_URL}" "${SUB_NAME}" "${UPLOAD_DATA}"
+  # echo "upload ok!"
 
+  sleep 100
+  done
 fi
