dfd9f55828593e06d542fbaa99a82985d77674bc
mytcgd <131883869+mytcgd@users.noreply.github.com>
2024-08-14 22:54:24 +0800

    Update up_s.sh

--- my/x/up_s.sh
+++ my/x/up_s.sh
@@ -1,32 +1,35 @@
-#!/usr/bin/env bash
+#!/bin/bash
 
 while true
 do
+
 upload_url_data() {
-    if [ $# -lt 3 ]; then
-        return 1
-    fi
+  if [ $# -lt 3 ]; then
+    return 1
+  fi
 
-    UPLOAD_URL="$1"
-    URL_NAME="$2"
-    URL_TO_UPLOAD="$3"
+  UPLOAD_URL="$1"
+  URL_NAME="$2"
+  URL_TO_UPLOAD="$3"
 
-    if command -v curl &> /dev/null; then
-
-        curl -s -o /dev/null -X POST -H "Content-Type: application/json" -d "{\"URL_NAME\": \"$URL_NAME\", \"URL\": \"$URL_TO_UPLOAD\"}" "$UPLOAD_URL"
-
-    elif command -v wget &> /dev/null; then
-
-        echo "{\"URL_NAME\": \"$URL_NAME\", \"URL\": \"$URL_TO_UPLOAD\"}" | wget --quiet --post-data=- --header="Content-Type: application/json" "$UPLOAD_URL" -O -
-
-    else
-        echo "Both curl and wget are not installed. Please install one of them to upload data."
-    fi
+  # 检查curl命令是否存在
+  if command -v curl &> /dev/null; then
+    curl -s -o /dev/null -X POST -H "Content-Type: application/json" -d "{\"URL_NAME\": \"$URL_NAME\", \"URL\": \"$URL_TO_UPLOAD\"}" "$UPLOAD_URL"
+  # 检查wget命令是否存在
+  elif command -v wget &> /dev/null; then
+    echo "{\"URL_NAME\": \"$URL_NAME\", \"URL\": \"$URL_TO_UPLOAD\"}" | wget --quiet --post-data=- --header="Content-Type: application/json" "$UPLOAD_URL" -O -
+  else
+    echo "Both curl and wget are not installed. Please install one of them to upload data."
+  fi
 }
 
 if [ -z "$ARGO_AUTH" ] && [ -z "$ARGO_DOMAIN" ]; then
   [ -s ${FILE_PATH}/boot.log ] && export ARGO_DOMAIN=$(cat ${FILE_PATH}/boot.log | grep -o "info.*https://.*trycloudflare.com" | sed "s@.*https://@@g" | tail -n 1)
+  # [ -s ${FILE_PATH}/boot.log ] && export ARGO_DOMAIN=$(cat boot.log | grep -oP '(?<=\|)(?!https://).*(?=\.com|$)' | tail -n 1)
+  # [ -s ${FILE_PATH}/boot.log ] && export ARGO_DOMAIN=$(cat ${FILE_PATH}/boot.log | grep -o "https://.*trycloudflare.com" | tail -n 1 | sed 's/https:\/\///')
 fi
+
+VMESS="{ \"v\": \"2\", \"ps\": \"vmess-${country_abbreviation}-${SUB_NAME}\", \"add\": \"${CF_IP}\", \"port\": \"${CFPORT}\", \"id\": \"${UUID}\", \"aid\": \"0\", \"scy\": \"none\", \"net\": \"ws\", \"type\": \"none\", \"host\": \"${ARGO_DOMAIN}\", \"path\": \"/${VMESS_WSPATH}?ed=2048\", \"tls\": \"tls\", \"sni\": \"${ARGO_DOMAIN}\", \"alpn\": \"\" }"
 
 vmess_url="vmess://$(echo "$VMESS" | base64 | tr -d '\n')"
 vless_url="vless://${UUID}@${CF_IP}:443?host=${ARGO_DOMAIN}&path=%2F${VLESS_WSPATH}%3Fed%3D2048&type=ws&encryption=none&security=tls&sni=${ARGO_DOMAIN}#vless-${country_abbreviation}-${SUB_NAME}"
@@ -41,9 +44,10 @@ else
   export UPLOAD_DATA="$vless_url"
   # export UPLOAD_DATA="$vmess_url\n$vless_url"
 fi
-# echo -e "${UPLOAD_DATA}"
 
 upload_url_data "${SUB_URL}" "${SUB_NAME}" "${UPLOAD_DATA}"
+# echo "upload ok!"
 
-sleep 300
+sleep 100
 done
+fi
