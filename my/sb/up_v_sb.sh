e1259cc3d0f391c7b74caaac98e4c4f52534c30e
mytcgd <131883869+mytcgd@users.noreply.github.com>
2024-08-17 05:34:38 +0800

    Update up_v_sb.sh

--- my/sb/up_v_sb.sh
+++ my/sb/up_v_sb.sh
@@ -25,21 +25,23 @@ if source /root/env.yml; then
   }
 
   [ -s ${FILE_PATH}/argo.log ] && export ARGO_DOMAIN=$(cat ${FILE_PATH}/argo.log | grep -o "info.*https://.*trycloudflare.com" | sed "s@.*https://@@g" | tail -n 1)
+  # [ -s ${FILE_PATH}/argo.log ] && export ARGO_DOMAIN=$(cat argo.log | grep -oP '(?<=\|)(?!https://).*(?=\.com|$)' | tail -n 1)
   # [ -s ${FILE_PATH}/argo.log ] && export ARGO_DOMAIN=$(cat ${FILE_PATH}/argo.log | grep -o "https://.*trycloudflare.com" | tail -n 1 | sed 's/https:\/\///')
 
-  VMESS="{ \"v\": \"2\", \"ps\": \"${country_abbreviation}-${SUB_NAME}\", \"add\": \"${CF_IP}\", \"port\": \"${CFPORT}\", \"id\": \"${UUID}\", \"aid\": \"0\", \"scy\": \"none\", \"net\": \"ws\", \"type\": \"none\", \"host\": \"${ARGO_DOMAIN}\", \"path\": \"/vmess?ed=2048\", \"tls\": \"tls\", \"sni\": \"${ARGO_DOMAIN}\", \"alpn\": \"\", \"fp\": \"randomized\"}"
+  # VMESS="{ \"v\": \"2\", \"ps\": \"${country_abbreviation}-${SUB_NAME}\", \"add\": \"${CF_IP}\", \"port\": \"${CFPORT}\", \"id\": \"${UUID}\", \"aid\": \"0\", \"scy\": \"none\", \"net\": \"ws\", \"type\": \"none\", \"host\": \"${ARGO_DOMAIN}\", \"path\": \"/vmess?ed=2048\", \"tls\": \"tls\", \"sni\": \"${ARGO_DOMAIN}\", \"alpn\": \"\", \"fp\": \"randomized\"}"
 
-  vmess_url="vmess://$(echo "$VMESS" | base64 | tr -d '\n')"
+  # vmess_url="vmess://$(echo "$VMESS" | base64 | tr -d '\n')"
+  vless_url="vless://${UUID}@${CF_IP}:${CFPORT}?host=${ARGO_DOMAIN}&path=%2Fvless%3Fed%3D2048&type=ws&encryption=none&security=tls&sni=${ARGO_DOMAIN}#vless-${country_abbreviation}-${SUB_NAME}"
   hysteria_url="hysteria2://${UUID}@${MYIP}:${HY2_PORT}/?sni=www.bing.com&alpn=h3&insecure=1#${country_abbreviation}-${SUB_NAME}"
   tuic_url="tuic://${UUID}:${tuicpass}@${MYIP}:${TUIC_PORT}?sni=www.bing.com&congestion_control=bbr&udp_relay_mode=native&alpn=h3&allow_insecure=1#${country_abbreviation}-${SUB_NAME}"
   reality_url="vless://${UUID}@${MYIP}:${REAL_PORT}?encryption=none&flow=xtls-rprx-vision&security=reality&sni=${SNI}&fp=chrome&pbk=${public_key}&type=tcp&headerType=none#${country_abbreviation}-${SUB_NAME}"
 
   if [ -n "$openreality" ] && [ "$openreality" != "0" ]; then
-    export UPLOAD_DATA="$vmess_url\n$hysteria_url\n$tuic_url\n$reality_url"
+    export UPLOAD_DATA="$vless_url\n$hysteria_url\n$tuic_url\n$reality_url"
   elif [ -z "$openreality" ]; then
-    export UPLOAD_DATA="$vmess_url"
+    export UPLOAD_DATA="$vless_url"
   else
-    export UPLOAD_DATA="$vmess_url"
+    export UPLOAD_DATA="$vless_url"
   fi
   # echo "upload ok !"
 
