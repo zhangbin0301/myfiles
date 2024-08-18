1cf0d798afb4f9e1e82332976e7e9dd8fa6cc147
mytcgd <131883869+mytcgd@users.noreply.github.com>
2024-08-18 21:52:59 +0800

    Update up_s_sb.sh

--- my/sb/up_s_sb.sh
+++ my/sb/up_s_sb.sh
@@ -1,4 +1,4 @@
-#!/usr/bin/env bash
+#!/bin/bash
 
 while true
 do
@@ -26,26 +26,26 @@ if [ -z "$ARGO_AUTH" ] && [ -z "$ARGO_DOMAIN" ]; then
   # [ -s ${FILE_PATH}/boot.log ] && export ARGO_DOMAIN=$(cat ${FILE_PATH}/boot.log | grep -o "https://.*trycloudflare.com" | tail -n 1 | sed 's/https:\/\///')
 fi
 
-VMESS="{ \"v\": \"2\", \"ps\": \"${country_abbreviation}-${SUB_NAME}\", \"add\": \"${CF_IP}\", \"port\": \"${CFPORT}\", \"id\": \"${UUID}\", \"aid\": \"0\", \"scy\": \"none\", \"net\": \"ws\", \"type\": \"none\", \"host\": \"${ARGO_DOMAIN}\", \"path\": \"/vmess?ed=2048\", \"tls\": \"tls\", \"sni\": \"${ARGO_DOMAIN}\", \"alpn\": \"\", \"fp\": \"randomized\"}"
+# VMESS="{ \"v\": \"2\", \"ps\": \"${country_abbreviation}-${SUB_NAME}\", \"add\": \"${CF_IP}\", \"port\": \"${CFPORT}\", \"id\": \"${UUID}\", \"aid\": \"0\", \"scy\": \"none\", \"net\": \"ws\", \"type\": \"none\", \"host\": \"${ARGO_DOMAIN}\", \"path\": \"/vmess?ed=2048\", \"tls\": \"tls\", \"sni\": \"${ARGO_DOMAIN}\", \"alpn\": \"\", \"fp\": \"randomized\"}"
 
-vmess_url="vmess://$(echo "$VMESS" | base64 | tr -d '\n')"
+# vmess_url="vmess://$(echo "$VMESS" | base64 | tr -d '\n')"
 vless_url="vless://${UUID}@${CF_IP}:${CFPORT}?host=${ARGO_DOMAIN}&path=%2Fvless%3Fed%3D2048&type=ws&encryption=none&security=tls&sni=${ARGO_DOMAIN}#vless-${country_abbreviation}-${SUB_NAME}"
 # hysteria_url="hysteria2://${UUID}@${MYIP}:${HY2_PORT}/?sni=www.bing.com&alpn=h3&insecure=1#${country_abbreviation}-${SUB_NAME}"
 # tuic_url="tuic://${UUID}:${tuicpass}@${MYIP}:${TUIC_PORT}?sni=www.bing.com&congestion_control=bbr&udp_relay_mode=native&alpn=h3&allow_insecure=1#${country_abbreviation}-${SUB_NAME}"
 reality_url="vless://${UUID}@${MYIP}:${REAL_PORT}?encryption=none&flow=xtls-rprx-vision&security=reality&sni=${SNI}&fp=chrome&pbk=${public_key}&type=tcp&headerType=none#${country_abbreviation}-${SUB_NAME}"
 
 if [ -n "$openreality" ] && [ "$openreality" != "0" ]; then
-  export UPLOAD_DATA="$vmess_url\n$vless_url\n$reality_url"
+  # export UPLOAD_DATA="$vmess_url\n$vless_url\n$reality_url"
   # export UPLOAD_DATA="$vmess_url\n$reality_url"
-  # export UPLOAD_DATA="$vless_url\n$reality_url"
+  export UPLOAD_DATA="$vless_url\n$reality_url"
 elif [ -z "$openreality" ]; then
-  export UPLOAD_DATA="$vmess_url\n$vless_url"
+  # export UPLOAD_DATA="$vmess_url\n$vless_url"
   # export UPLOAD_DATA="$vmess_url"
-  # export UPLOAD_DATA="$vless_url"
+  export UPLOAD_DATA="$vless_url"
 else
-  export UPLOAD_DATA="$vmess_url\n$vless_url"
+  # export UPLOAD_DATA="$vmess_url\n$vless_url"
   # export UPLOAD_DATA="$vmess_url"
-  # export UPLOAD_DATA="$vless_url"
+  export UPLOAD_DATA="$vless_url"
 fi
 # echo -e "${UPLOAD_DATA}"
 
