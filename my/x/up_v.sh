3b57e7df25ad367d731f5cc2248ce835f5423c96
mytcgd <131883869+mytcgd@users.noreply.github.com>
2024-09-09 01:29:19 +0800

    Update up_v.sh

--- my/x/up_v.sh
+++ my/x/up_v.sh
@@ -13,8 +13,10 @@ if source /root/env.yml; then
     URL_NAME="$2"
     URL_TO_UPLOAD="$3"
 
+    # 检查curl命令是否存在
     if command -v curl &> /dev/null; then
       curl -s -o /dev/null -X POST -H "Content-Type: application/json" -d "{\"URL_NAME\": \"$URL_NAME\", \"URL\": \"$URL_TO_UPLOAD\"}" "$UPLOAD_URL"
+    # 检查wget命令是否存在
     elif command -v wget &> /dev/null; then
       echo "{\"URL_NAME\": \"$URL_NAME\", \"URL\": \"$URL_TO_UPLOAD\"}" | wget --quiet --post-data=- --header="Content-Type: application/json" "$UPLOAD_URL" -O -
     else
@@ -27,19 +29,19 @@ if source /root/env.yml; then
 
   VMESS="{ \"v\": \"2\", \"ps\": \"${country_abbreviation}-${SUB_NAME}\", \"add\": \"${CF_IP}\", \"port\": \"${CFPORT}\", \"id\": \"${UUID}\", \"aid\": \"0\", \"scy\": \"none\", \"net\": \"ws\", \"type\": \"none\", \"host\": \"${ARGO_DOMAIN}\", \"path\": \"/vmess?ed=2048\", \"tls\": \"tls\", \"sni\": \"${ARGO_DOMAIN}\", \"alpn\": \"\", \"fp\": \"randomized\"}"
 
-  vmess_url="vmess://$(echo "$VMESS" | base64 | tr -d '\n')"
+  # vmess_url="vmess://$(echo "$VMESS" | base64 | tr -d '\n')"
   vless_url="vless://${UUID}@${CF_IP}:${CFPORT}?host=${ARGO_DOMAIN}&path=%2F${VLESS_WSPATH}%3Fed%3D2048&type=ws&encryption=none&security=tls&sni=${ARGO_DOMAIN}#vless-${country_abbreviation}-${SUB_NAME}"
+  reality_url="vless://${UUID}@${MYIP}:${REAL_PORT}?encryption=none&flow=xtls-rprx-vision&security=reality&sni=${SNI}&fp=chrome&pbk=${PublicKey}&sid=${shortid}&type=tcp&headerType=none#${country_abbreviation}-${SUB_NAME}"
 
-  if [ -n "$openreality" ] && [ "$openreality" != "0" ]; then
-    reality_url="vless://${UUID}@${MYIP}:${REAL_PORT}?encryption=none&flow=xtls-rprx-vision&security=reality&sni=${SNI}&fp=chrome&pbk=${PublicKey}&sid=${shortid}&type=tcp&headerType=none#${country_abbreviation}-${SUB_NAME}"
-    export UPLOAD_DATA="$vless_url\n$reality_url"
-  elif [ -z "$openreality" ]; then
-    export UPLOAD_DATA="$vless_url"
-    # export UPLOAD_DATA="$vmess_url\n$vless_url"
-  else
-    export UPLOAD_DATA="$vless_url"
-    # export UPLOAD_DATA="$vmess_url\n$vless_url"
+  # UPLOAD_DATA="$vmess_url\n$vless_url"
+  UPLOAD_DATA="$vless_url"
+
+  if [ -n "$REAL_PORT" ]; then
+    UPLOAD_DATA="$UPLOAD_DATA\n$reality_url"
   fi
+
+  export UPLOAD_DATA
+  # echo -e "${UPLOAD_DATA}"
 
   upload_url_data "${SUB_URL}" "${SUB_NAME}" "${UPLOAD_DATA}"
   # echo "upload ok!"
