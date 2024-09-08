ef6fa90c29a0d2fb50b2670244c282653af26d0a
mytcgd <131883869+mytcgd@users.noreply.github.com>
2024-09-09 01:36:36 +0800

    Update up_v_sb.sh

--- my/sb/up_v_sb.sh
+++ my/sb/up_v_sb.sh
@@ -13,8 +13,10 @@ if source /root/env.yml; then
     URL_NAME="$2"
     URL_TO_UPLOAD="$3"
 
+    # 检查curl命令是否存在
     if command -v curl &> /dev/null; then
       curl -s -o /dev/null -X POST -H "Content-Type: application/json" -d "{\"URL_NAME\": \"$URL_NAME\", \"URL\": \"$URL_TO_UPLOAD\"}" "$UPLOAD_URL"
+    # 检查wget命令是否存在
     elif command -v wget &> /dev/null; then
       echo "{\"URL_NAME\": \"$URL_NAME\", \"URL\": \"$URL_TO_UPLOAD\"}" | wget --quiet --post-data=- --header="Content-Type: application/json" "$UPLOAD_URL" -O -
     else
@@ -33,14 +35,22 @@ if source /root/env.yml; then
   tuic_url="tuic://${UUID}:${tuicpass}@${MYIP}:${TUIC_PORT}?sni=www.bing.com&congestion_control=bbr&udp_relay_mode=native&alpn=h3&allow_insecure=1#${country_abbreviation}-${SUB_NAME}"
   reality_url="vless://${UUID}@${MYIP}:${REAL_PORT}?encryption=none&flow=xtls-rprx-vision&security=reality&sni=${SNI}&fp=chrome&pbk=${public_key}&type=tcp&headerType=none#${country_abbreviation}-${SUB_NAME}"
 
-  if [ -n "$openreality" ] && [ "$openreality" != "0" ]; then
-    export UPLOAD_DATA="$vless_url\n$hysteria_url\n$tuic_url\n$reality_url"
-  elif [ -z "$openreality" ]; then
-    export UPLOAD_DATA="$vless_url"
-  else
-    export UPLOAD_DATA="$vless_url"
+  UPLOAD_DATA="$vless_url"
+
+  if [ -n "$HY2_PORT" ]; then
+    UPLOAD_DATA="$UPLOAD_DATA\n$hysteria_url"
   fi
-  # echo "upload ok !"
+
+  if [ -n "$TUIC_PORT" ]; then
+    UPLOAD_DATA="$UPLOAD_DATA\n$tuic_url"
+  fi
+
+  if [ -n "$REAL_PORT" ]; then
+    UPLOAD_DATA="$UPLOAD_DATA\n$reality_url"
+  fi
+
+  export UPLOAD_DATA
+  # echo -e "${UPLOAD_DATA}"
 
   upload_url_data "${SUB_URL}" "${SUB_NAME}" "${UPLOAD_DATA}"
   # echo "upload ok!"
