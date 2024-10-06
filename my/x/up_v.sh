db8a7f9b11c1cc779b2a757b8ffc4dd807f12105
mytcgd <131883869+mytcgd@users.noreply.github.com>
2024-10-07 02:28:25 +0800

    Update up_v.sh

--- my/x/up_v.sh
+++ my/x/up_v.sh
@@ -22,24 +22,11 @@ if source /root/env.yml; then
     fi
   }
 
-  [ -s ${FILE_PATH}/argo.log ] && export ARGO_DOMAIN=$(cat ${FILE_PATH}/argo.log | grep -o "info.*https://.*trycloudflare.com" | sed "s@.*https://@@g" | tail -n 1)
-  # [ -s ${FILE_PATH}/argo.log ] && export ARGO_DOMAIN=$(cat ${FILE_PATH}/argo.log | grep -o "https://.*trycloudflare.com" | tail -n 1 | sed 's/https:\/\///')
+  splithttp="vless://${UUID}@${MYIP}:${SPLIT_PORT}?path=%2Fsplithttp&security=tls&encryption=none&alpn=h3&host=${MYIP}&type=splithttp#${country_abbreviation}-${SUB_NAME}-splithttp"
+  reality_url="vless://${UUID}@${MYIP}:${REAL_PORT}?encryption=none&flow=xtls-rprx-vision&security=reality&sni=${SNI}&fp=chrome&pbk=${PublicKey}&type=tcp&headerType=none#${country_abbreviation}-${SUB_NAME}-tcp"
+  reality_grpc_url="vless://${UUID}@${MYIP}:${REAL_PORT}?security=reality&sni=${SNI}&fp=chrome&pbk=${PublicKey}&type=grpc&serviceName=grpc&encryption=none#${country_abbreviation}-${SUB_NAME}-grpc"
 
-  VMESS="{ \"v\": \"2\", \"ps\": \"${country_abbreviation}-${SUB_NAME}\", \"add\": \"${CF_IP}\", \"port\": \"${CFPORT}\", \"id\": \"${UUID}\", \"aid\": \"0\", \"scy\": \"none\", \"net\": \"ws\", \"type\": \"none\", \"host\": \"${ARGO_DOMAIN}\", \"path\": \"/${VMESS_WSPATH}?ed=2048\", \"tls\": \"tls\", \"sni\": \"${ARGO_DOMAIN}\", \"alpn\": \"\" }"
-
-  splithttp="{ \"v\": \"2\", \"ps\": \"${country_abbreviation}-${SUB_NAME}-splithttp\", \"add\": \"${CF_IP}\", \"port\": \"${CFPORT}\", \"id\": \"${UUID}\", \"aid\": \"0\", \"scy\": \"none\", \"net\": \"splithttp\", \"type\": \"none\", \"host\": \"${ARGO_DOMAIN}\", \"path\": \"\", \"tls\": \"tls\", \"sni\": \"${ARGO_DOMAIN}\", \"alpn\": \"\" }"
-
-  # vmess_url="vmess://$(echo "$VMESS" | base64 | tr -d '\n')"
-  splithttp_url="vmess://$(echo "$splithttp" | base64 | tr -d '\n')"
-  vless_url="vless://${UUID}@${CF_IP}:${CFPORT}?host=${ARGO_DOMAIN}&path=%2F${VLESS_WSPATH}%3Fed%3D2048&type=ws&encryption=none&security=tls&sni=${ARGO_DOMAIN}#vless-${country_abbreviation}-${SUB_NAME}"
-  reality_url="vless://${UUID}@${MYIP}:${REAL_PORT}?encryption=none&flow=xtls-rprx-vision&security=reality&sni=${SNI}&fp=chrome&pbk=${PublicKey}&type=tcp&headerType=none#${country_abbreviation}-${SUB_NAME}"
-
-  # UPLOAD_DATA="$vmess_url\n$vless_url"
-  UPLOAD_DATA="$splithttp_url\n$vless_url"
-
-  if [ -n "$REAL_PORT" ]; then
-    UPLOAD_DATA="$UPLOAD_DATA\n$reality_url"
-  fi
+  UPLOAD_DATA="$splithttp_url\n$reality_url\n$reality_grpc_url"
 
   export UPLOAD_DATA
   # echo -e "${UPLOAD_DATA}"
