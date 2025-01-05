6d625b24400634054a00967c6486b71d408f28e9
mytcgd <131883869+mytcgd@users.noreply.github.com>
2025-01-06 07:38:18 +0800

    Update up_s.sh

--- my/x/up_s.sh
+++ my/x/up_s.sh
@@ -1,7 +1,23 @@
 #!/bin/bash
 
-while true
-do
+general_upload_data() {
+  # vmess_url="vmess://$(echo "$VMESS" | base64 | tr -d '\n')"
+  vless_url="vless://${UUID}@${CF_IP}:${CFPORT}?host=${ARGO_DOMAIN}&path=%2F${VLESS_WSPATH}%3Fed%3D2048&type=ws&encryption=none&security=tls&sni=${ARGO_DOMAIN}#${country_abbreviation}-${SUB_NAME}"
+
+  # UPLOAD_DATA="$vmess_url\n$vless_url"
+  UPLOAD_DATA="$vless_url"
+
+  if [ -n "$REAL_PORT" ] && [ -n "$shortid" ]; then
+    realitytcp_url="vless://${UUID}@${MYIP}:${REAL_PORT}?encryption=none&flow=xtls-rprx-vision&security=reality&sni=${SNI}&fp=chrome&pbk=${PublicKey}&sid=${shortid}&type=tcp&headerType=none#${country_abbreviation}-${SUB_NAME}-realtcp"
+    UPLOAD_DATA="$UPLOAD_DATA\n$realitytcp_url"
+  elif [ -n "$REAL_PORT" ] && [ -z "$shortid" ]; then
+    realitytcp_url="vless://${UUID}@${MYIP}:${REAL_PORT}?encryption=none&flow=xtls-rprx-vision&security=reality&sni=${SNI}&fp=chrome&pbk=${PublicKey}&type=tcp&headerType=none#${country_abbreviation}-${SUB_NAME}-realtcp"
+    UPLOAD_DATA="$UPLOAD_DATA\n$realitytcp_url"
+  fi
+
+  export UPLOAD_DATA
+  # echo -e "${UPLOAD_DATA}"
+}
 
 upload_url_data() {
   if [ $# -lt 3 ]; then
@@ -21,31 +37,21 @@ upload_url_data() {
   fi
 }
 
-if [ -z "$ARGO_AUTH" ] && [ -z "$ARGO_DOMAIN" ]; then
+if [ -n "$ARGO_DOMAIN" ] && [ -n "$ARGO_AUTH" ]; then
+  general_upload_data
+  upload_url_data "${SUB_URL}" "${SUB_NAME}" "${UPLOAD_DATA}"
+else
+  while true
+  do
+
   [ -s ${FILE_PATH}/boot.log ] && export ARGO_DOMAIN=$(cat ${FILE_PATH}/boot.log | grep -o "info.*https://.*trycloudflare.com" | sed "s@.*https://@@g" | tail -n 1)
   # [ -s ${FILE_PATH}/boot.log ] && export ARGO_DOMAIN=$(cat ${FILE_PATH}/boot.log | grep -o "https://.*trycloudflare.com" | tail -n 1 | sed 's/https:\/\///')
-fi
 
-# vmess_url="vmess://$(echo "$VMESS" | base64 | tr -d '\n')"
-vless_url="vless://${UUID}@${CF_IP}:${CFPORT}?host=${ARGO_DOMAIN}&path=%2F${VLESS_WSPATH}%3Fed%3D2048&type=ws&encryption=none&security=tls&sni=${ARGO_DOMAIN}#${country_abbreviation}-${SUB_NAME}"
+  general_upload_data
+  upload_url_data "${SUB_URL}" "${SUB_NAME}" "${UPLOAD_DATA}"
+  # echo "upload ok!"
 
-# UPLOAD_DATA="$vmess_url\n$vless_url"
-UPLOAD_DATA="$vless_url"
-
-if [ -n "$REAL_PORT" ] && [ -n "$shortid" ]; then
-  realitytcp_url="vless://${UUID}@${MYIP}:${REAL_PORT}?encryption=none&flow=xtls-rprx-vision&security=reality&sni=${SNI}&fp=chrome&pbk=${PublicKey}&sid=${shortid}&type=tcp&headerType=none#${country_abbreviation}-${SUB_NAME}-realtcp"
-  UPLOAD_DATA="$UPLOAD_DATA\n$realitytcp_url"
-elif [ -n "$REAL_PORT" ] && [ -z "$shortid" ]; then
-  realitytcp_url="vless://${UUID}@${MYIP}:${REAL_PORT}?encryption=none&flow=xtls-rprx-vision&security=reality&sni=${SNI}&fp=chrome&pbk=${PublicKey}&type=tcp&headerType=none#${country_abbreviation}-${SUB_NAME}-realtcp"
-  UPLOAD_DATA="$UPLOAD_DATA\n$realitytcp_url"
-fi
-
-export UPLOAD_DATA
-# echo -e "${UPLOAD_DATA}"
-
-upload_url_data "${SUB_URL}" "${SUB_NAME}" "${UPLOAD_DATA}"
-# echo "upload ok!"
-
-sleep 300
-done
+  sleep 300
+  done
+  fi
 fi
