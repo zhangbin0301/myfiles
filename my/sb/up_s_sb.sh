8095fd516bd1a598e11534aaec09ef848a9f6a13
mytcgd <131883869+mytcgd@users.noreply.github.com>
2025-01-06 14:53:51 +0800

    Update up_s_sb.sh

--- my/sb/up_s_sb.sh
+++ my/sb/up_s_sb.sh
@@ -1,7 +1,29 @@
 #!/bin/bash
 
-while true
-do
+general_upload_data() {
+  # vmess_url="vmess://$(echo "$VMESS" | base64 | tr -d '\n')"
+  vless_url="vless://${UUID}@${CF_IP}:${CFPORT}?host=${ARGO_DOMAIN}&path=%2Fvless%3Fed%3D2048&type=ws&encryption=none&security=tls&sni=${ARGO_DOMAIN}#${country_abbreviation}-${SUB_NAME}"
+  hysteria_url="hysteria2://${UUID}@${MYIP}:${HY2_PORT}/?sni=www.bing.com&alpn=h3&insecure=1#${country_abbreviation}-${SUB_NAME}"
+  tuic_url="tuic://${UUID}:${tuicpass}@${MYIP}:${TUIC_PORT}?sni=www.bing.com&congestion_control=bbr&udp_relay_mode=native&alpn=h3&allow_insecure=1#${country_abbreviation}-${SUB_NAME}"
+  reality_url="vless://${UUID}@${MYIP}:${REAL_PORT}?encryption=none&flow=xtls-rprx-vision&security=reality&sni=${SNI}&fp=chrome&pbk=${public_key}&type=tcp&headerType=none#${country_abbreviation}-${SUB_NAME}-realitytcp"
+
+  UPLOAD_DATA="$vless_url"
+
+  if [ -n "$HY2_PORT" ]; then
+    UPLOAD_DATA="$UPLOAD_DATA\n$hysteria_url"
+  fi
+
+  if [ -n "$TUIC_PORT" ]; then
+    UPLOAD_DATA="$UPLOAD_DATA\n$tuic_url"
+  fi
+
+  if [ -n "$REAL_PORT" ]; then
+    UPLOAD_DATA="$UPLOAD_DATA\n$reality_url"
+  fi
+
+  export UPLOAD_DATA
+  # echo -e "${UPLOAD_DATA}"
+}
 
 upload_url_data() {
   if [ $# -lt 3 ]; then
@@ -21,36 +43,22 @@ upload_url_data() {
   fi
 }
 
-if [ -z "$ARGO_AUTH" ] && [ -z "$ARGO_DOMAIN" ]; then
+if [ -n "$ARGO_DOMAIN" ] && [ -n "$ARGO_AUTH" ]; then
+  general_upload_data
+  upload_url_data "${SUB_URL}" "${SUB_NAME}" "${UPLOAD_DATA}"
+else
+  while true
+  do
+
   [ -s ${FILE_PATH}/boot.log ] && export ARGO_DOMAIN=$(cat ${FILE_PATH}/boot.log | grep -o "info.*https://.*trycloudflare.com" | sed "s@.*https://@@g" | tail -n 1)
   # [ -s ${FILE_PATH}/boot.log ] && export ARGO_DOMAIN=$(cat ${FILE_PATH}/boot.log | grep -o "https://.*trycloudflare.com" | tail -n 1 | sed 's/https:\/\///')
+
+  general_upload_data
+  upload_url_data "${SUB_URL}" "${SUB_NAME}" "${UPLOAD_DATA}"
+
+  sleep 300
+  done
+  fi
 fi
 
-# vmess_url="vmess://$(echo "$VMESS" | base64 | tr -d '\n')"
-vless_url="vless://${UUID}@${CF_IP}:${CFPORT}?host=${ARGO_DOMAIN}&path=%2Fvless%3Fed%3D2048&type=ws&encryption=none&security=tls&sni=${ARGO_DOMAIN}#${country_abbreviation}-${SUB_NAME}"
-hysteria_url="hysteria2://${UUID}@${MYIP}:${HY2_PORT}/?sni=www.bing.com&alpn=h3&insecure=1#${country_abbreviation}-${SUB_NAME}"
-tuic_url="tuic://${UUID}:${tuicpass}@${MYIP}:${TUIC_PORT}?sni=www.bing.com&congestion_control=bbr&udp_relay_mode=native&alpn=h3&allow_insecure=1#${country_abbreviation}-${SUB_NAME}"
-reality_url="vless://${UUID}@${MYIP}:${REAL_PORT}?encryption=none&flow=xtls-rprx-vision&security=reality&sni=${SNI}&fp=chrome&pbk=${public_key}&type=tcp&headerType=none#${country_abbreviation}-${SUB_NAME}-realitytcp"
-
-UPLOAD_DATA="$vless_url"
-
-if [ -n "$HY2_PORT" ]; then
-  UPLOAD_DATA="$UPLOAD_DATA\n$hysteria_url"
-fi
-
-if [ -n "$TUIC_PORT" ]; then
-  UPLOAD_DATA="$UPLOAD_DATA\n$tuic_url"
-fi
-
-if [ -n "$REAL_PORT" ]; then
-  UPLOAD_DATA="$UPLOAD_DATA\n$reality_url"
-fi
-
-export UPLOAD_DATA
-# echo -e "${UPLOAD_DATA}"
-
-upload_url_data "${SUB_URL}" "${SUB_NAME}" "${UPLOAD_DATA}"
 # echo "upload ok!"
-
-sleep 300
-done
