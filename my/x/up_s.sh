868c1177fce48f77fa383a838f3178771022a436
mytcgd <131883869+mytcgd@users.noreply.github.com>
2025-01-14 16:10:51 +0800

    Update up_s.sh

--- my/x/up_s.sh
+++ my/x/up_s.sh
@@ -37,9 +37,8 @@ upload_url_data() {
   fi
 }
 
-general_upload_data
-
 if [ -n "$ARGO_DOMAIN" ] && [ -n "$ARGO_AUTH" ]; then
+  general_upload_data
   upload_url_data "${SUB_URL}" "${SUB_NAME}" "${UPLOAD_DATA}"
 elif [ -n "$MY_DOMAIN" ]; then
   upload_url_data "${SUB_URL}" "${SUB_NAME}" "${UPLOAD_DATA}"
@@ -50,6 +49,7 @@ else
   [ -s ${FILE_PATH}/boot.log ] && export ARGO_DOMAIN=$(cat ${FILE_PATH}/boot.log | grep -o "info.*https://.*trycloudflare.com" | sed "s@.*https://@@g" | tail -n 1)
   # [ -s ${FILE_PATH}/boot.log ] && export ARGO_DOMAIN=$(cat ${FILE_PATH}/boot.log | grep -o "https://.*trycloudflare.com" | tail -n 1 | sed 's/https:\/\///')
 
+  general_upload_data
   upload_url_data "${SUB_URL}" "${SUB_NAME}" "${UPLOAD_DATA}"
 
   sleep 100
