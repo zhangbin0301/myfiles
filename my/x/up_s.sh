c904ef2bcf2953f945819f0609475b881a29ab7b
mytcgd <131883869+mytcgd@users.noreply.github.com>
2025-01-17 20:00:12 +0800

    Update up_s.sh

--- my/x/up_s.sh
+++ my/x/up_s.sh
@@ -46,8 +46,10 @@ else
   while true
   do
 
-  [ -s ${FILE_PATH}/boot.log ] && export ARGO_DOMAIN=$(cat ${FILE_PATH}/boot.log | grep -o "info.*https://.*trycloudflare.com" | sed "s@.*https://@@g" | tail -n 1)
-  # [ -s ${FILE_PATH}/boot.log ] && export ARGO_DOMAIN=$(cat ${FILE_PATH}/boot.log | grep -o "https://.*trycloudflare.com" | tail -n 1 | sed 's/https:\/\///')
+  if [ -s "${FILE_PATH}/boot.log" ]; then
+    export ARGO_DOMAIN=$(cat ${FILE_PATH}/argo.log | grep -o "info.*https://.*trycloudflare.com" | sed "s@.*https://@@g" | tail -n 1)
+    # export ARGO_DOMAIN=$(cat ${FILE_PATH}/argo.log | grep -o "https://.*trycloudflare.com" | tail -n 1 | sed 's/https:\/\///')
+  fi
 
   general_upload_data
   upload_url_data "${SUB_URL}" "${SUB_NAME}" "${UPLOAD_DATA}"
